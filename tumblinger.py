38d6487551ce704ff806c9c921217c4131c4c6ba
bgnori <bgnori@gmail.com>
2012-06-23 19:44:24 +0900

    made first post from tumblr. 	modified:   tumblinger.py

--- tumblinger.py
+++ tumblinger.py
@@ -1,7 +1,12 @@
 
 from lingr import Session
+from pyblr import Pyblr
 
+from oauth2 import Client, Consumer, Token
+import yaml
 import sys
+
+
 
 if len(sys.argv) < 3: 
     print 'need user and password'
@@ -18,12 +23,18 @@ else:
 
 Session.api_key = "kzRHJn"
 
-with Session(**config) as s:
-    to_remove = []
-    s.enter("computer_science")
-    s.enter("bgnori")
-    for i in range(4, len(sys.argv)):
-        j = s.say(sys.argv[i])
-        to_remove.append(j['message']['id'])
+with open("tumblr-oauth.txt") as f:
+    d = yaml.load(f)
+    client = Client(Consumer(d["consumer_key"], d["consumer_secret"]), Token(d["user_key"], d["user_secret"]))
+    t = Pyblr(client)
 
+    with Session(**config) as s:
+        to_remove = []
+        s.enter("bgnori")
+    
+        j = t.posts('bgnori.tumblr.com')
+        for post in reversed(j['posts']):
+            if 'photos' in post:
+                for x in post['photos']:
+                    s.say(x['original_size']['url'])
 
